bdbfe6ef45a460c15811eb64100af05d9eab5faa
hieppu0772543313 <140375597+hieppu0772543313@users.noreply.github.com>
2024-05-10 08:33:28 +0700

    Update todo.js

--- exercise3/todo.js
+++ exercise3/todo.js
@@ -1,60 +1,18 @@
-import React, { useEffect, useState } from "react"
-import { Appbar, Button, TextInput } from "react-native-paper"
-import { FlatList, View } from "react-native"
-import Todo from "./todo"
-import firebase from "@react-native-firebase/app"
-import firestore from "@react-native-firebase/firestore"
+import React from 'react';
+import firestore from '@react-native-firebase/firestore';
+import {List} from 'react-native-paper';
 
-
-const App = ()=>{
-  const [todo, setTodo] = useState('');
-  const [loading, setLoading] = useState(true);
-  const [todos, setTodos] = useState([]);
-  const ref = firestore().collection('todos');
-  async function addTodo(){
-    await ref.add({
-      title:todo,
-      complete:false,
-    });
-    setTodo('');
+export default function Todo({id, title, complete}) {
+  async function toggleComplete() {
+    await firestore().collection('todos').doc(id).update({complete: !complete});
   }
-  useEffect(()=>{
-    return ref.onSnapshot(querySnapshot => {
-      const list =[];
-      querySnapshot.forEach(doc=>{
-        const {title, complete} = doc.data();
-        list.push({
-          id: doc.id,
-          title,
-          complete,
-        })
-      })
-      setTodos(list);
-      if (loading){
-        setLoading(false);
-      }
-    });
-  });
-  if (loading){
-    return null;
-  }
-  return(
-    <View style={{flex:1}}>
-      <Appbar>
-        <Appbar.Content title={' TODOs List'}/>
-      </Appbar>
-      <FlatList
-        style={{flex:1}}
-        data={todos}
-        keyExtractor={(item)=>item.id}
-        renderItem={({item})=> <Todo {...item} /> }
-      />
-      <TextInput label={'New Todo'} 
-        value={todo}
-        onChangeText={(text) =>setTodo(text)}/>
-      <Button onPress={addTodo}>Add TODO</Button>
-    </View>
+  return (
+    <List.Item
+      title={title}
+      onPress={() => toggleComplete()}
+      left={props => (
+        <List.Icon {...props} icon={complete ? 'check' : 'cancel'} />
+      )}
+    />
   );
 }
-
-export default App;
